eda64205d7ecc1340f190cc7096bf6faa7472b3f
UlianaOr <Ouliana9@gmail.com>
2022-01-10 12:25:06 +0100

    asd9

--- script.js
+++ script.js
@@ -15,6 +15,11 @@ function jump () {
 }
 
 let isAlive = setInterval (function() {
-    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"))
-    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"))
-})
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+
+
+if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
+    alert ("GAME OVER!!")
+}
+}, 10 )
